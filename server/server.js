9d2983a745e6420046e04efc2740aa51415389c2
jasonyoung00 <96235225+jasonyoung00@users.noreply.github.com>
2023-10-16 16:38:54 +0000

    server.js now listening on port 8000 rather than 3000

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is Working!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server.js listening on port ${port}`)
 })
