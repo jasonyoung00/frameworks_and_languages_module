710aff48a2ceee5c393fd2dc3dd7a368250c5bbd
jasonyoung00 <96235225+jasonyoung00@users.noreply.github.com>
2023-10-25 22:22:03 +0000

    tweaks to GET items, progress on DELETE and GET item/

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 8000
+//const cors = require('cors')
 
 app.use(express.json())
+//app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Server Working!')
   res.status(200)
+  console.log("200 OK");
 })
 
 let items =
@@ -30,29 +33,54 @@ let items =
 
 app.post('/item', (req, res) => {
   if (Object.keys(req.body).toString() != "user_id,keywords,description,lat,lon") {
-    return res.status(405).json({"message": "Missing Fields"})
+    return res.status(405).json({"message": "Missing Input Fields"})
   }
   else
   {
-    const Time_Date = new Date().toISOString();
+    const date_from = new Date().toISOString();
     const unique_Num = Math.random();
     
     req.body.id = unique_Num
-    req.body['fromDate'] = Time_Date
+    req.body['fromDate'] = date_from
     req.body['id'] = unique_Num;
 
     items.push(req.body)
     res.status(201).json()
-    console.log("brick")
+    console.log("201 Created")
   }
 })
 
 app.get('/items', (req, res) => {
   res.json(items)
+  console.log("GET items")
 })
 
-app.get('/item/', (req, res) => {
-  res.json('Hello World!')
+app.get('/item/:itemId', (req, res) => {
+  const itemId = parseFloat(req.params.itemId);
+  const itemSearched = items.find(itemSearched => itemSearched.itemId === itemId)
+
+  if(!itemSearched)
+  {
+    console.log("404 Not Found");
+    res.status(404).json('Failed Search');
+  }
+
+  console.log("User Not Found" + itemSearched);
+  res.status(200).json(itemSearched);
+  return;
+})
+
+app.delete('item/:itemId', (req, res) => {
+  const itemId = parseFloat(req.params.itemId);
+  const itemIndex = items.findIndex(item => item.itemId === itemId);
+
+  if (itemIndex === -1) {
+    return res.status(404).json({message: 'Item Not Found'});
+  }
+
+  items.splice(itemIndex, -1);
+  console.log("204 No Content: " + itemId);
+  res.status(204).json
 })
 
 app.listen(port, () => {
