a835cf5939d909b430e76a9aed27bf8050a9b8d4
jasonyoung00 <96235225+jasonyoung00@users.noreply.github.com>
2023-11-06 16:53:52 +0000

    different data structure option commented

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ app.get('/', (req, res) => {
   console.log("200 OK");
 })
 
+/*var items = {
+  1: {
+    "id": 1,
+    "user_id": "user1234",
+    "keywords": ["hammer", "nails", "tools"],
+    "description": "A hammer and nails set",
+    "lat": 1,
+    "lon": 1,
+    "date_from": "2021-11-22T08:22:39.067408",
+  }
+};*/
+
 let items =
 [
   {
