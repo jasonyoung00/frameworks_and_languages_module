9065c7e9b361ab22c67cae9bcd21a4b8e8cfd725
jasonyoung00 <96235225+jasonyoung00@users.noreply.github.com>
2023-11-13 15:52:23 +0000

    curl commands on the server page, small GUI progress on the client page

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Server Working!')
-  res.status(200)
+  res.sendFile('client.html', {root: __dirname})
   console.log("200 OK");
 })
 
